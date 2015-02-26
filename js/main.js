e686fb8306acf808eb3184f5f2fa30ebbbbd2c58
kody smith <kodysmith2010@gmail.com>
2015-02-26 14:46:52 -0800

    Update main.js
    making require calls relative to app ... not root

--- js/main.js
+++ js/main.js
@@ -9,9 +9,9 @@
 
 require.config({
   paths: {
-    jquery: 'libs/jquery/jquery-min',
-    underscore: 'libs/underscore/underscore-min',
-    backbone: 'libs/backbone/backbone-min',
+    jquery: '../libs/jquery/jquery-min',
+    underscore: '../libs/underscore/underscore-min',
+    backbone: '../libs/backbone/backbone-min',
     templates: '../templates'
   }
 
